09fccbafcbe4bbd1efdb99811d0dfd6a0566c626
Robert <harezlak.robert7@gmail.com>
2016-07-14 15:26:55 +0200

    implementation of NumeralTranslation

--- roman_numerals/lib/roman_numerals.rb
+++ roman_numerals/lib/roman_numerals.rb
@@ -1,19 +1,40 @@
 class NumeralTranslator
-  ROMAN_NUMERALS_TRANSLAIONS = {
-    1000 => 'M',
-    500 => 'D',
-    400 => 'CD',
-    100 => 'C',
-    90 => 'XC',
-    50 => 'L',
-    40 => 'XL',
-    10 => 'X',
-    9 => 'IX',
-    5 => 'V',
-    4 => 'IV',
-    1 => 'I'
-  }.freeze
+  attr_accessor :arabic_input
 
-  def translate(_input)
+  ROMAN_NUMERALS_TRANSLATIONS = { 1000 => 'M', 500 => 'D', 100 => 'C', 50 => 'L', 10 => 'X', 5 => 'V', 1 => 'I' }.freeze
+  ROMAN_EXCEPTIONS_TRANSLATIONS = { 'DCCCC' => 'CM', 'CCCC' => 'CD', 'LXXXX' => 'XC', 'XXXX' => 'XL', 'VIIII' => 'IX',
+                                    'IIII' => 'IV' }.freeze
+
+  def initialize(arabic_input)
+    @arabic_input = arabic_input
+    @roman_number = ''
+  end
+
+  def translate
+    result = ''
+    ROMAN_NUMERALS_TRANSLATIONS.each { |_number, letter| result << extract_all_possible(letter) }
+    convert_exceptions(result)
+  end
+
+  private
+
+  def convert_exceptions(roman_digit)
+    ROMAN_EXCEPTIONS_TRANSLATIONS.each { |k, v| roman_digit.gsub!(k, v) }
+    roman_digit
+  end
+
+  def extract_all_possible(letter)
+    letters_count = amount_of_contained_letters(letter)
+    @arabic_input -= letters_count * arabic_equivalent(letter)
+    letter * letters_count
+  end
+
+  def amount_of_contained_letters(letter)
+    (@arabic_input / arabic_equivalent(letter)).to_i
+  end
+
+  def arabic_equivalent(letter)
+    return unless ROMAN_NUMERALS_TRANSLATIONS.value?(letter)
+    ROMAN_NUMERALS_TRANSLATIONS.find { |_a_num, r_digit| r_digit == letter }.first
   end
 end
--- roman_numerals/spec/lib/roman_numerals_spec.rb
+++ roman_numerals/spec/lib/roman_numerals_spec.rb
@@ -1,31 +1,38 @@
 require 'roman_numerals'
+require 'pry'
 
 describe NumeralTranslator do
-  let(:roman_numerals) { NumeralTranslator.new }
+  let(:roman_numerals) { NumeralTranslator.new(input) }
 
   describe 'constant' do
-    pending 'returns correct roman numerals translations' do
-      expect(NumeralTranslator::ROMAN_NUMERALS_TRANSLAIONS).to eql(1 => 'I', 5 => 'V', 10 => 'X', 50 => 'L', 100 => 'C',
-                                                                   500 => 'D', 1000 => 'M')
+    it 'returns correct roman numerals translations' do
+      expect(NumeralTranslator::ROMAN_NUMERALS_TRANSLATIONS).to eql(1 => 'I', 5 => 'V', 10 => 'X', 50 => 'L', 100 => 'C',
+                                                                    500 => 'D', 1000 => 'M')
     end
   end
 
   describe '#translate' do
     context 'when number end with "9"' do
-      pending 'returns corrrect roman number' do
-        expect(roman_numerals.translate(159)).to eql 'CLIX'
+      let(:input) { 159 }
+
+      it 'returns corrrect roman number' do
+        expect(roman_numerals.translate).to eql 'CLIX'
       end
     end
 
     context 'when number contains "990"' do
-      pending 'returns corret roman number' do
-        expect(roman_numerals.translate(3992)).to eql 'MMMCMXCII'
+      let(:input) { 3992 }
+
+      it 'returns corret roman number' do
+        expect(roman_numerals.translate).to eql 'MMMCMXCII'
       end
     end
 
     context 'when number contains "444"' do
-      pending 'returns correct roman number' do
-        expect(roman_numerals.translate(444)).to eql 'CDXLIV'
+      let(:input) { 444 }
+
+      it 'returns correct roman number' do
+        expect(roman_numerals.translate).to eql 'CDXLIV'
       end
     end
   end
