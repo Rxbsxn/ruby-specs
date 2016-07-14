187d568c272ee6d57b1db5b26160ac0dc23db807
Robert <harezlak.robert7@gmail.com>
2016-07-14 11:50:21 +0200

    pendings

---
+++ roman_numerals/spec/lib/roman_numerals_spec.rb
@@ -0,0 +1,18 @@
+require 'roman_numerals'
+
+describe RomanNumerals do
+  describe '.to_roman' do
+    pending '1000 should give M'
+    pending '500 should give D'
+    pending '100 should give C'
+    pending '50 should give L'
+    pending '10 should give X'
+    pending '5 should give V'
+    pending '1 should give I'
+    pending '900 should give CM'
+    pending '400 should give CD'
+    pending '90 should give XC'
+    pending '40 should give XL'
+    pending '9 should give IX'
+  end
+end
